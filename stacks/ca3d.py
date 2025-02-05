369e2c6c5e1ab30b9bfcf30f044a6e82f47bc9fc
Gabriele Lagani <gabriele.lagani@gmail.com>
2025-02-05 14:45:14 +0100

    Stacks ready

--- stacks/ca3d.py
+++ stacks/ca3d.py
@@ -1,5 +1,5 @@
 datasets = ['ucf101', 'hmdb51', 'kinetics400', 'ucf101clp', 'hmdb51clp', 'kinetics400clp']
-precisions = ['f16, f16-qat', 'f16-sqr', 'f16-root', 'f32']
+precisions = ['f16', 'f16-qat', 'f16-sqr', 'f16-root', 'f32']
 all = {d: [] for d in datasets}
 
 for d in datasets:
